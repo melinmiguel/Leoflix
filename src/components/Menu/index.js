7222654af6a11276c04ca9a8bb05af06ce82c7f6
melinmiguel <melinmiguel@gmail.com>
2020-10-02 19:27:53 -0300

    Inicio do Projeto

---
+++ src/components/Menu/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo_Leoflix.png'
+import ButtonLink from '../Menu/Components/ButtonLink';
+import './Menu.css'
+import Button from '../Button'
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt=" Leoflix logo" />
+            </a>
+
+            <Button as="a" className="ButtonLink" href="/">
+                Novo Video!
+            </Button>
+        </nav>
+    );
+}
+export default Menu;
